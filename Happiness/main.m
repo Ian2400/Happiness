47651bd89f59f5dc49f8b384eb3377138758998c
Your Name <Ian2400@gmail.com>
2012-07-09 23:07:17 -0400

    Initial Commit

---
+++ Happiness/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Happiness
+//
+//  Created by Ian Pytlarz on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HappinessAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HappinessAppDelegate class]));
+    }
+}
